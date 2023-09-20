0ece6136ca897063105cfe211c352d04fd260e75
anushujan satkunarasa <43997407+anushujan@users.noreply.github.com>
2023-09-20 09:28:57 +0530

    email updated

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,20 +1,42 @@
 import "./FormStyles.css";
+import emailjs from "@emailjs/browser";
 
-import React from "react";
+import React, { useRef } from "react";
 
 const Form = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_hh5tjeb",
+        "template_8likieb",
+        form.current,
+        "F67Wnf9KqJj76higo"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
   return (
     <div className="form">
-      <form>
+      <form ref={form} onSubmit={sendEmail}>
         <label>Full Name</label>
-        <input type="text" name="" id="" />
+        <input type="text" name="from_name" id="" />
         <label>Email</label>
-        <input type="email" name="" id="" />
+        <input type="email" name="from_email" id="" />
         <label>Subject</label>
-        <input type="text" name="" id="" />
+        <input type="text" name="subject" id="" />
         <label>Message</label>
         <textarea
-          name=""
+          name="message"
           id=""
           cols="30"
           rows="6"
