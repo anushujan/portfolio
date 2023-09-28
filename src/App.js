45034f15d06b6378c7dde8f0c1eea95635e68b7a
anushujan satkunarasa <43997407+anushujan@users.noreply.github.com>
2023-09-28 10:39:45 +0530

    whatsapp connect

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Contact from './routes/Contact'
 import { Route, Routes } from 'react-router-dom'
 
 
+
 function App() {
   return (
     <>
--- src/routes/Contact.js
+++ src/routes/Contact.js
@@ -1,9 +1,12 @@
-import React, { useEffect } from 'react'
+import React, { useEffect } from "react";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
-import BackImg2 from '../components/backImg2';
-import Form from '../components/Form';
-
+import BackImg2 from "../components/backImg2";
+import Form from "../components/Form";
+// import { render } from 'react-dom';
+import { FloatingWhatsApp } from "react-floating-whatsapp";
+// import ReactWhatsapp from 'react-whatsapp';
+// import {ava} from '../assets/logo512.png'
 
 const Contact = () => {
   useEffect(() => {
@@ -11,12 +14,22 @@ const Contact = () => {
   }, []);
   return (
     <div>
-      <Navbar/>
-      <BackImg2 heading="CONTACT" text="Let's have a chat"/>
-      <Form/>
-      <Footer/>
+      <Navbar />
+      <BackImg2 heading="CONTACT" text="Let's have a chat" />
+      <Form />
+      <FloatingWhatsApp
+        placeholder="Type a message"
+        phoneNumber="+94 771827347"
+        accountName="Anushujan"
+        avatar="logo512.png"
+        statusMessage="online"
+        chatMessage="Hello there! "
+        messageDelay={2}
+        darkMode
+      />
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
